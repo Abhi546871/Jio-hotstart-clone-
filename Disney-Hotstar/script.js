7ba25a5ecb6c398e3500a2137c925d9e99e121a6
Abhi546871 <165551816+Abhi546871@users.noreply.github.com>
2025-04-12 19:07:04 +0530

    Add files via upload

---
+++ Disney-Hotstar/script.js
@@ -0,0 +1,17 @@
+// let ml = 1458;
+// let fullwidth = document.getElementById("slider").offsetWidth;
+// let remainingwidth = fullwidth;
+// document.getElementById("sleft").addEventListener("click",function(){
+//   console.log("Hello left working");
+// })
+// document.getElementById("sright").addEventListener("click",function(){
+//     if(remainingwidth>ml){
+//         document.getElementById("slider").style.marginLeft=-ml+"px";
+//         remainingwidth = remainingwidth - 1715;
+//         ml = ml + 1715;
+//     }
+//     else if(remainingwidth<ml){
+//         document.getElementById("slider").style.marginLeft=-(ml+remainingwidth)+"px";
+//     }
+    
+// })
